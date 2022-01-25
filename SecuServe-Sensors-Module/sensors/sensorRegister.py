14801b51af467f55701dcb38aaa6b3e3c370db9c
NicholasBlackburn1 <nickblackburn02@gmail.com>
2022-01-25 08:35:07 -0500

    removed old sensor stuff

--- SecuServe-Sensors-Module/sensors/sensorRegister.py
+++ SecuServe-Sensors-Module/sensors/sensorRegister.py
@@ -11,54 +11,5 @@ from pydispatch import dispatcher
 
 class sensorRegister(object):
 
-    mainDevice = None
-    network = None
-
-    #Sets up Zwave device
-    def setupZwaveGateWay(self,device):
-        # Initialise openzwave.
-        consoleLog.info("setting up Zwave Options")
-
-        self.mainDevice = ZWaveController(device, user_path=".")
-        self.mainDevice.set_console_output(False)
-        self.mainDevice.lock()
-
-        self.network = ZWaveNetwork(self.mainDevice)
-       
-
-        consoleLog.PipeLWine_Ok("setup Zwave options successfully...")
-
-    
-
-    # when node updates
-    def node_update(network, node):
-         consoleLog.PipeLine_Data('Node update : {}.'.format(node))
-
-    # when valuse update
-    def value_update(network, node, value):
-        consoleLog.PipeLine_Data('Value update : {}.'.format(value))
-
-    # when Control Network updates
-    def ctrl_message(state, message, network, controller):
-       consoleLog.PipeLine_Data('Controller message : {}.'.format(message))
-
-    # starts the zwave netowrk 
-    def startNetwork(self):
-        self.network.start()
-        
-
-    # Connect to events
-    def value_updated(network, node, value):
-        now = datetime.now().isoformat()
-        consoleLog.PipeLine_Data("%s: Value updated node_id: <%d>, label <%s> new value <%s> instance %d" % ( now, node.node_id, value.label, str(value.data), value.instance))
-
-
-
-    #allowwws me to run stuff for the sensors
-    def pullData(self):
-        consoleLog.info("Startiting network data from zwave")
-    
-        while self.network.state != self.network.STATE_READY:
-            sleep(1)
-    
+  
 
